805d65b467e880271a69c182c90976500be58922
Sarah Naas <sjnaas@gmail.com>
2019-11-25 22:01:57 -0800

    all functions working except final command

--- liri.js
+++ liri.js
@@ -4,28 +4,7 @@ var inquirer = require("inquirer");
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
 
-//var spotify = new Spotify(keys);
-
 var convertedUserInput = [];
-
-conversion();
-
-function conversion() {
-  let message = "";
-  for (let i = 2; i < process.argv.length; i++) {
-    if (i != process.argv.length - 1) {
-      message += process.argv[i] + "+";
-    } else {
-      message += process.argv[i];
-    }
-  }
-
-  convertedUserInput.push(message);
-}
-
-//console.log(keys);
-
-getMovie();
 
 function getMovie() {
   axios
@@ -35,17 +14,17 @@ function getMovie() {
         "&y=&plot=short&apikey=trilogy"
     )
     .then(function(response) {
-      console.log(response.data.Title);
-      console.log(response.data.Year);
-      console.log(response.data.Ratings[0].Value);
-      console.log(response.data.Ratings[1].Value);
-      console.log(response.data.Country);
-      console.log(response.data.Language);
-      console.log(response.data.Plot);
-      console.log(response.data.Actors);
+      console.log("Movie Title: " + response.data.Title);
+      console.log("Year: " + response.data.Year);
+      console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+      console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+      console.log("Country: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Plot: " + response.data.Plot);
+      console.log("Actors: " + response.data.Actors);
     });
 }
-//getBands();
+
 function getBands() {
   axios
     .get(
@@ -59,61 +38,99 @@ function getBands() {
       console.log(response.data[0].datetime);
     });
 }
-// inquirer
-//   .prompt({
-//     type: "list",
-//     message: "What would you like to do?",
-//     choices: ["Concert", "Spotify", "Movie", "Do what it says"],
-//     name: "select-command"
-//   })
-//   .then(res => {
-//     console.log("I got lost on a tangent");
-//   });
 
-// const concertSearch = () => {
-//   inquirer
-//     .prompt({
-//       type: "input",
-//       message: "Enter artist's name here: ",
-//       name: "concert"
-//     })
-//     .then(res => {
-//       console.log("concert info");
-//     });
-// };
+function searchSpotify() {
+  var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
 
-// const spotifySearch = () => {
-//   inquirer
-//     .prompt({
-//       type: "input",
-//       message: "Enter song name here: ",
-//       name: "spotify-search"
-//     })
-//     .then(res => {
-//       console.log("song info");
-//     });
-// };
+  spotify.search({ type: "track", query: convertedUserInput }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
 
-// const movieSearch = () => {
-//   inquirer
-//     .prompt({
-//       type: "input",
-//       message: "Enter movie name here: ",
-//       name: "movie"
-//     })
-//     .then(res => {
-//       console.log("movie info");
-//     });
-// };
+    console.log("Artist: " + data.tracks.items[0].artists[0].name);
+    console.log("Song: " + data.tracks.items[0].name);
+    console.log("URL: " + data.tracks.items[0].preview_url);
+    console.log("Album: " + data.tracks.items[0].album.name);
+  });
+}
 
-// const command = () => {
-//   inquirer
-//     .prompt({
-//       type: "input",
-//       message: "Enter command here: ",
-//       name: "command-name"
-//     })
-//     .then(res => {
-//       console.log("command info");
-//     });
-// };
+inquirer
+  .prompt({
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["Concert", "Spotify", "Movie", "Do what it says"],
+    name: "selectCommand"
+  })
+  .then(res => {
+    res.selectCommand === "Concert"
+      ? concertSearch()
+      : res.selectCommand === "Spotify"
+      ? spotifySearch()
+      : res.selectCommand === "Movie"
+      ? movieSearch()
+      : res.selectCommand === "Do what it says"
+      ? command()
+      : process.exit();
+  });
+
+function concertSearch() {
+  inquirer
+    .prompt({
+      type: "input",
+      message: "Enter artist's name here: ",
+      name: "concert"
+    })
+    .then(res => {
+      var str = res.concert;
+      var replaced = str.split(" ").join("+");
+      console.log(replaced);
+      convertedUserInput.push(replaced);
+      getBands();
+    });
+}
+
+const spotifySearch = () => {
+  inquirer
+    .prompt({
+      type: "input",
+      message: "Enter song name here: ",
+      name: "spotifySearch"
+    })
+    .then(res => {
+      convertedUserInput.push(res.spotifySearch);
+      searchSpotify();
+    });
+};
+
+const movieSearch = () => {
+  inquirer
+    .prompt({
+      type: "input",
+      message: "Enter movie name here: ",
+      name: "movie"
+    })
+    .then(res => {
+      var str = res.movie;
+      var replaced = str.split(" ").join("+");
+      convertedUserInput.push(replaced);
+      getMovie();
+    });
+};
+
+const command = () => {
+  inquirer
+    .prompt({
+      type: "input",
+      message: "Enter command here: ",
+      name: "command-name"
+    })
+    .then(res => {
+      console.log("command info");
+    });
+};
